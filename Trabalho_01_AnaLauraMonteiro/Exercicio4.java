96211712e06ee7b6f6ca1d304cc929ff38355d0f
aninha-ah <analauramonteirooliveira@gmail.com>
2023-03-20 13:45:13 +0000

    salvar

--- Trabalho_01_AnaLauraMonteiro/Exercicio4.java
+++ Trabalho_01_AnaLauraMonteiro/Exercicio4.java
@@ -4,21 +4,22 @@ import java.util.Scanner;
 public class Exercicio4 {
     public static void main(String[] args) {
 
-    float salario;
+    float salario, sm;
         Scanner input = new Scanner(System.in); 
 
         System.out.println("Informe o valor do salario: ");
-        salario, s1, s2, s3 = input.nextDouble(); 
-
-s1 = salario * 1.15;
-s2= salario * 1.10;
-s3 = salario * 1.05;
+        salario = input.nextDouble(); 
 
         if (salario >= 1000 && salario < 2000) { 
-            System.out.print("\n" + nome1 + " é  a pessoa mais velha é, com: \n" + idade1 );
+           sm = salario * 1.15;
+            System.out.print("\n" + "Seu novo salário é": \n" + salario);
+
         } else if (salario >= 2000 && salario > 3000) {
-            System.out.print("\n" + nome2 + " é a pessoa mais velha é, com: \n" + idade2);
+            sm = salario * 1.10;
+            System.out.print("\n" Seu novo salário é: \n" + salario);
+
            }   else {
+            sm = salario * 1.05;
                 System.out.print("As duas pessoas tem a mesma idade: \n");
             }
 }
